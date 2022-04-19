cad87ce5c5fb21ae493ffae35f51412d03916c3c
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-04-19 06:41:45 +0100

    feat: add checksum validator to updates
    Signed-off-by: BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>

--- backend/updater.go
+++ backend/updater.go
@@ -10,32 +10,40 @@ import (
 )
 
 func Update(version string) error {
-	latest, found, err := selfupdate.DetectLatest("BitsOfAByte/MarketXIV")
 
+	updater, _ := selfupdate.NewUpdater(selfupdate.Config{Validator: &selfupdate.ChecksumValidator{UniqueFilename: "checksums.txt"}})
+	latest, found, err := updater.DetectLatest("BitsOfAByte/MarketXIV")
+
+	// Unknown error occurred, abort update process.
 	if err != nil {
 		return fmt.Errorf("error occurred while detecting version: %v", err)
 	}
 
+	// Specified OS or Architechture is not supported.
 	if !found {
 		return fmt.Errorf("latest version for %s/%s could not be found from GitHub repository", runtime.GOOS, runtime.GOARCH)
 	}
 
+	// No update is available for the current version.
 	if latest.LessOrEqual(version) {
 		fmt.Printf("You currently have the latest version! (%s)", version)
 		return nil
 	}
 
+	// Find the current executable's path.
 	exe, err := os.Executable()
 
+	//  Could not find the executable's path, abort update process.
 	if err != nil {
 		return errors.New("could not locate executable path")
 	}
 
+	// Perform the update.
 	if err := selfupdate.UpdateTo(latest.AssetURL, latest.AssetName, exe); err != nil {
 		return fmt.Errorf("error occurred while updating binary: %v", err)
 	}
 
-	fmt.Printf("Successfully updated to version %s", latest.Version())
+	fmt.Printf("Successfully updated to version %s (Arch: %s, OS: %s) ", latest.Version(), latest.Arch, latest.OS)
 
 	return nil
 }
