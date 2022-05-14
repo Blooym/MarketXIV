960a64289bbc1eb2f29bb2d34bc164c741d1ee75
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-14 17:41:05 +0100

    build: adjust ldflag setting

---
+++ build/info.go
@@ -0,0 +1,12 @@
+package build
+
+var (
+	// Information about the build itself, used for debugging.
+	Version = "v0.0.0"
+	Commit  = "unknown"
+	Date    = "unknown"
+
+	// Supabase environment is set at build time
+	SUPABASE_URL = "https://itwxdznbfhlzjxvmhwib.supabase.co"
+	SUPABASE_KEY = "unset"
+)
