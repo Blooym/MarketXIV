d99b75c3e7721a0f61b2980c94f385641e086299
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-25 13:13:09 +0100

    build: publish to pkg managers

--- cmd/update.go
+++ cmd/update.go
@@ -6,6 +6,9 @@ Copyright © 2022 BitsOfAByte
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/BitsOfAByte/marketxiv/backend"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +18,15 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update to the latest version of the app if available",
 	Run: func(cmd *cobra.Command, args []string) {
-		backend.Update(rootCmd.Version)
+		forceFlag := cmd.Flag("force").Value.String()
+		if forceFlag == "true" {
+			backend.Update(rootCmd.Version)
+		} else {
+			fmt.Println("WARNING! You should not use the app-update command unless you have a manual installation.")
+			fmt.Println("If you are trying to update the app and have installed it with a package manager, use that instead.")
+			fmt.Println("If you are ABSOLUTELY sure you want to update, use the --force flag.")
+			os.Exit(1)
+		}
 	},
 }
 
