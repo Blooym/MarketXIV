99396fdf2f090f7c2f7f8a9498a492ce1bdb7879
Harmony <19539165+BitsOfAByte@users.noreply.github.com>
2022-08-07 23:25:52 +0100

    chore: remove unused build info

--- build/info.go
+++ build/info.go
@@ -5,8 +5,4 @@ var (
 	Version = "v0.0.0"
 	Commit  = "unknown"
 	Date    = "unknown"
-
-	// Supabase environment is set at build time
-	SUPABASE_URL = "https://itwxdznbfhlzjxvmhwib.supabase.co"
-	SUPABASE_KEY = "unset"
 )
